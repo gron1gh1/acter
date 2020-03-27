c2c83bf129bb7c0166b195ed4fcef4caa3d504c0
unknown <gron1gh1@ajou.ac.kr>
2020-03-28 00:20:39 +0900

    Add Component drag => main panel pass

--- design-tool/src/App.js
+++ design-tool/src/App.js
@@ -1,9 +1,9 @@
 import './antd.css'
 import './styles.css'
-import React, { Fragment, useState, useRef } from 'react'
+import React, { Fragment, useState, createContext, useContext,useRef } from 'react'
 import { Keyframes, animated } from 'react-spring/renderprops'
 import { useSpring, animated as a } from 'react-spring';
-import { Layout, Avatar, Form, Input, Button, Checkbox,Row,Col } from 'antd'
+import { Layout, Avatar, Form, Input, Button, Checkbox, Row, Col } from 'antd'
 import { MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined, LockOutlined } from '@ant-design/icons';
 import delay from 'await-delay';
 import Draggable from 'react-draggable';
@@ -31,15 +31,15 @@ const Sidebar_Color = 'rgb(59,160,233)';
 const Content_Color = 'rgb(16,142,233)';
 const Header_Color = 'rgb(125,188,234)';
 
-function Layout_1({ opacity }) {
+function Layout_1({ style }) {
   return (
-    <Layout style={{ opacity }}>
+    <Layout style={style}>
       <Header style={{ background: `${Header_Color}` }}>Header</Header>
       <Layout>
         <Layout.Content style={{ background: `${Content_Color}` }}>
           Content
           </Layout.Content>
-        <Sider style={{ background: `${Sidebar_Color}` }}>Sider</Sider>
+
       </Layout>
       <Footer style={{ background: `${Header_Color}` }}>Footer</Footer>
     </Layout>
@@ -47,20 +47,22 @@ function Layout_1({ opacity }) {
 }
 
 function Layout_item({ item }) {
+  const show_layout = useContext(AppContext);
   const [start, AniControl] = useState(0);
-  const ani_props = useSpring({ opacity: start ? 0 : 1 })
+  const boxRef = useRef();
+  const ani_props = useSpring({ opacity: start ? 0 : 1 });
 
   const [opacity, SetOpa] = useState(1);
   function drag_start(e) {
     SetOpa(0.5);
   }
   function drag_ing(e, data) {
-
   }
   function drag_stop(e, data) {
     SetOpa(1);
-    if (data.x > 270) {
+    if (data.x > boxRef.current.offsetWidth) {
       AniControl(1);
+      show_layout.SetLayout(true);
       console.log('침범');
     }
   }
@@ -69,7 +71,7 @@ function Layout_item({ item }) {
     <Draggable onStart={drag_start}
       onDrag={drag_ing}
       onStop={drag_stop}>
-      <a.div style={ani_props}>
+      <a.div ref={boxRef} style={ani_props}>
         <Layout_1 opacity={opacity} />
       </a.div>
     </Draggable>
@@ -100,30 +102,29 @@ const items = [
       className="login-form-button"
       children="Log in"
     />
-  </Fragment>,<Layout_item/>
-  
+  </Fragment>,
+  <Layout_item />
+
 ]
 
-export default function App() {
+function Main() {
   const [open, SetOpen] = useState();
   const toggle = () => SetOpen(!open);
-  const contentProps = useSpring({
-    left: open ? 220 : 0
-  });
-
+  const show_layout = useContext(AppContext);
   const state = open === undefined
     ? 'close'
     : open
       ? 'open'
       : 'close';
+
+const ani_props = useSpring({ opacity: show_layout.layout ? 1 : 0 });
   return (
-    <div >
-      <Row style={{position: 'absolute', width: '100%', height: '100%'}}>
-        <Col span={4} 
-              className='sidebar' style={{background:'white',overflow:'visible',zIndex:99} }>
-        <MenuFoldOutlined className="sidebar-toggle" onClick={toggle} style={{position:'absolute',right:10,top:0}}/>
-      <Sidebar native state={state} >
-        {({ x }) => (
+    <Row style={{ position: 'absolute', width: '100%', height: '100%' }}>
+      <Col span={4}
+        className='sidebar' style={{ background: 'white', overflow: 'visible', zIndex: 99 }}>
+        <MenuFoldOutlined className="sidebar-toggle" onClick={toggle} style={{ position: 'absolute', right: 10, top: 0 }} />
+        <Sidebar native state={state} >
+          {({ x }) => (
             <animated.div
               style={{
                 transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
@@ -145,19 +146,42 @@ export default function App() {
                     </Form.Item>
                   </animated.div>
                 )}
-               
+
               </Content>
             </animated.div>
-             
-        )}
-        
-      </Sidebar>
-      </Col>
-      <Col span={20} style={{background:'lightgray'}}>
-        ASD
-      </Col>
-      </Row>
-    </div>
 
+          )}
+
+        </Sidebar>
+      </Col>
+      <Col span={20} style={{ background: 'lightgray' }}>
+
+        {show_layout.layout &&
+          <a.div style={ani_props}>
+            <Layout_1 style={{ height: '100vh' }} />
+          </a.div>
+        }
+
+      </Col>
+    </Row>
+
+
+  )
+}
+
+const AppContext = createContext();
+export default function App() {
+  const [layout, SetLayout] = useState(false);
+
+  const data = {
+    layout, SetLayout
+  }
+
+  return (
+    <AppContext.Provider value={data}>
+      <Fragment>
+        <Main />
+      </Fragment>
+    </AppContext.Provider>
   )
 }
