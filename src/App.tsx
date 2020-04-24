a79df41a2c9887724263a1e82a55e8c10478aa88
unknown <gron1gh1@ajou.ac.kr>
2020-04-24 23:17:53 +0900

    (Add) Radio & (Modified) MainView changed flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState, Fragment } from 'react';
-import { Row, Col} from 'antd';
+import { Row, Col, Radio } from 'antd';
 import { Droppable, DragDropContext, DropResult } from 'react-beautiful-dnd';
 import { useSelector, useDispatch } from 'react-redux';
-import { IDragging, IMainState,IMenuState } from './Interface';
+import { IDragging, IMainState, IMenuState } from './Interface';
 import { ActionCreators } from './reducer';
 import Sidebar from './Sidebar';
-import {ItemList} from './StaticData';
+import { ItemList } from './StaticData';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 const getMainViewStyle = (isDraggingOver: boolean): React.CSSProperties => ({
     background: isDraggingOver ? "lightblue" : "white",
@@ -16,16 +17,16 @@ const getMainViewStyle = (isDraggingOver: boolean): React.CSSProperties => ({
 
 function App() {
     const [dragging, SetDragging] = useState<IDragging>({ state: false, item: null }); // Item information at start of drag
-    
+
 
     const dispatch = useDispatch();
     const MainLayout: (React.ReactElement | null) = useSelector((state: IMainState) => state.Layout); // Get Data from Reducer to this 
 
     function onDragEnd(result: DropResult) {
-        const { source, destination, type} = result;
+        const { source, destination, type } = result;
         let _type = type as keyof IMenuState;
         if (!destination || source.droppableId === destination.droppableId) return;
-        
+
         console.log(type, dragging.item, destination.droppableId);
         dragging.item && ItemList[_type] && dispatch(ActionCreators.addComponent(destination.droppableId, ItemList[_type][dragging.item]));
         SetDragging({ state: false, item: null });
@@ -42,8 +43,15 @@ function App() {
                     <Col flex="264px" style={{ background: 'white', overflowY: 'hidden', height: '100vh' }}>
                         <Sidebar />
                     </Col>
-                    <Col flex="auto" style={{ background: 'lightgray' }}>
-                        <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, margin: 'auto', width: '1024px', height: '768px', background: 'white' }}>
+                    <Col flex="auto" style={{ background: 'lightgray', height:'100vh',
+                    display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+                        <div style={{marginLeft:'-870px'}}>
+                        <Radio.Group defaultValue="PREVIEW" buttonStyle="solid">
+                            <Radio.Button value="PREVIEW">PREVIEW</Radio.Button>
+                            <Radio.Button value="CODE">CODE</Radio.Button>
+                        </Radio.Group>
+                        </div>
+                        <div style={{ width: '1024px', height: '768px', background: 'white' }}>
                             <Droppable droppableId="Layout" type="MAINVIEW">
                                 {(provided, snapshot) => (
                                     <div
