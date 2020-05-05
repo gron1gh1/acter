977ed4e8df72f02e573ab7c62a162897c74d6076
unknown <gron1gh1@ajou.ac.kr>
2020-05-05 15:10:30 +0900

    (Modify) react-live

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,11 @@ import {
     LiveProvider,
     LiveEditor,
     LiveError,
-    LivePreview
+    LivePreview,
+    
 } from 'react-live'
+import { PrismTheme} from 'prism-react-renderer';
+
 
 import ReactDOMServer from 'react-dom/server';
 import reactElementToJSXString from 'react-element-to-jsx-string';
@@ -56,15 +59,86 @@ function MainView({ MainLayout }: IMainView) {
     )
 }
 
-
+const reactLiveHome : PrismTheme= {
+    plain: {
+      color: '#e7d2ed'
+    },
+    styles: [
+      {
+        types: ['prolog', 'comment', 'doctype', 'cdata'],
+        style: {
+          color: 'hsl(30, 20%, 50%)'
+        }
+      },
+      {
+        types: ['property', 'tag', 'boolean', 'number', 'constant', 'symbol'],
+        style: { color: '#f677e1' }
+      },
+      {
+        types: ['attr-name', 'string', 'char', 'builtin', 'insterted'],
+        style: {
+          color: 'hsl(75, 70%, 70%)'
+        }
+      },
+      {
+        types: [
+          'operator',
+          'entity',
+          'url',
+          'string',
+          'variable',
+          'language-css'
+        ],
+        style: {
+          color: 'hsl(40, 90%, 70%)'
+        }
+      },
+      {
+        types: ['deleted'],
+        style: {
+          color: 'rgb(255, 85, 85)'
+        }
+      },
+      {
+        types: ['italic'],
+        style: {
+          fontStyle: 'italic'
+        }
+      },
+      {
+        types: ['important', 'bold'],
+        style: {
+          fontWeight: 'bold'
+        }
+      },
+      {
+        types: ['regex', 'important'],
+        style: {
+          color: '#e90'
+        }
+      },
+      {
+        types: ['atrule', 'attr-value', 'keyword'],
+        style: {
+          color: '#f677e1'
+        }
+      },
+      {
+        types: ['punctuation', 'symbol'],
+        style: {
+          opacity: 0.7
+        }
+      }
+    ]
+  };
 const scope = {Radio};
 function CodeView() {
     return (
-        <div style={getMainViewStyle(false)}>
-            <LiveProvider code="<strong>Hello World!</strong>" scope={scope}>
-                <LiveEditor />
+        <div style={{width:'1024px',minHeight:'240px',display:'flex',flexDirection:'column'}}>
+            <LiveProvider code="<strong>Hello World!</strong>" scope={scope} >
+                <LiveEditor style={{background:'rgb(50,42,56)',caretColor: 'rgb(197,200,198)'}}/>
                 <LiveError />
-                <LivePreview />
+                <LivePreview/>
             </LiveProvider>
         </div>
     )
