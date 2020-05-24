27be606dcb1e39ca75b147fe0cad53611d840c34
unknown <gron1gh1@ajou.ac.kr>
2020-05-24 20:22:22 +0900

    (Modify) getNPM

--- src/CodeView.tsx
+++ src/CodeView.tsx
@@ -9,6 +9,7 @@ import {
 
 } from 'react-live'
 import { ISelect } from './Interface';
+import * as antd from 'antd';
 import * as Item from './ItemComponent';
 import {getNPM} from './util';
 
@@ -24,7 +25,7 @@ const StyledLiveEditor = styled(LiveEditor)`
     caret-color: rgb(197,200,198);
 `;
 
-const ModuleToObject = (npm_lib : object) => { // NPM Module or Local Module To Object (ex: {Button : function Button(...)}
+const ModuleToObject = (npm_lib : object) => { // Local Module To Object (ex: {Button : function Button(...)}
     let res = {};
     let obj_names = Object.keys(npm_lib);
     Object.values(npm_lib).forEach((v, i) => {
@@ -32,16 +33,77 @@ const ModuleToObject = (npm_lib : object) => { // NPM Module or Local Module To 
     })
     return res;
 }
+/* 
+class App extends React.Component {
+ 
+  constructor()
+  {
+  super();
+  this.state = {
+    current: 'mail',
+  };
+  }
+  handleClick(e) {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
 
-const scope2 = { LoginItem: Item.LoginItem };
+  render() {
+    return (
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="mail" >
+          Navigation One
+        </Menu.Item>
+        <Menu.Item key="app">
+          Navigation Two
+        </Menu.Item>
+        <Menu.Item key="alipay">
+          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+            Navigation Four - Link
+          </a>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
 
+function App2()
+{
+	const [current,setCurrent] = useState('mail');
+	function handleClick(e)
+	{
+		setCurrent(e.key);
+	}
+	return (
+      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="mail" >
+          Navigation One
+        </Menu.Item>
+        <Menu.Item key="app">
+          Navigation Two
+        </Menu.Item>
+        <Menu.Item key="alipay">
+          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+            Navigation Four - Link
+          </a>
+        </Menu.Item>
+      </Menu>
+    );
+}
+
+render(
+ <App/>
+)
+ */
 export function CodeView() {
     const CodeData: string = useSelector((state: ISelect) => state.codeReducer.Code as string); // Get Data from Reducer to this
     const [scope,SetScope] = useState({});
     useEffect(() => {
         let npm_lib = {};
-        getNPM('antd').then((v : any)=> SetScope(ModuleToObject(v)));
-      //  SetScope(get_scope(Item));
+        getNPM('antd@4.1.1').then((v : any)=> SetScope({...ModuleToObject(v),...{useState},...{useEffect}}));
+     //   SetScope(ModuleToObject(antd));
     }, [])
 
     useEffect(()=>{
@@ -49,7 +111,11 @@ export function CodeView() {
     },[scope])
     return (
         <WrapperCode>
-            <LiveProvider code={CodeData ? CodeData : "Hello World"} scope={scope}>
+            <LiveProvider noInline code={CodeData ? CodeData : `render (
+                <div>
+
+                </div>
+            )`} scope={scope}>
                 <StyledLiveEditor />
                 <LiveError />
                 <LivePreview style={{ background: 'white' }} />
